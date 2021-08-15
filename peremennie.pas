aa59b9923b90c3bf07fd67b0af30c049e5643c13
pochtalon <vivobook@mail.com>
2021-08-15 10:54:30 +0300

    delet

--- peremennie.pas
+++
@@ -1,7 +0,0 @@
-program peremennie;
-begin
-   writeln('Типы ЗНАКОВЫХ целых чисел: shortint, integer, longint, int64');
-   writeln('Типы БЕЗЗНАКОВЫХ целых чисел: byte, word, longword, qword');
-   writeln('Соответственно: однобайтное, двухбайтное, четырехбайтное и восьмибайтное');
-   writeln('Числа с плавающей точкой обычно относят к типу: real')
-end.
